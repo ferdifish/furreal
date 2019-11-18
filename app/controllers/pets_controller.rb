7a73db5af555f06a8c43bf98f9354fd86a665d31
Laura Pham <l.pham2311@gmail.com>
2019-11-18 16:38:14 +0000

    Added user to pet creation

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,9 @@ class PetsController < ApplicationController
   end
 
   def create
-    Pet.create(pet_params)
+    pet = Pet.new(pet_params)
+    pet.user = current_user
+    pet.save
     redirect_to pets_path
   end
 
