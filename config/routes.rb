b18d0aa2c6bf60bfd565f39aa92aaaea0754de68
Ferdinand Vermersch <ferdi.vermersch@gmail.com>
2019-11-18 15:19:53 +0000

    added routes and added rented pets alias

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :bookings, only: [:index, :show, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :pets, only: [:index, :show, :new, :create] do
+   resources :bookings, only: [:new, :create]
+  end
+  resources :reviews, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
