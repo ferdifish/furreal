d2298161c068caefb4475d6d494064a3b70d2042
Laura Pham <l.pham2311@gmail.com>
2019-11-19 12:03:46 +0000

    Refreshed pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,8 +14,11 @@ class PetsController < ApplicationController
   def create
     pet = Pet.new(pet_params)
     pet.user = current_user
-    pet.save
-    redirect_to pets_path
+    if pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
   end
 
   def destroy
